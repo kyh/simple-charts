82f10c54055327fbc3d4d30bd2b3d90e76b12909
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-06-06 00:17:51 -0400

    Added base chart functions

--- app/scripts/charts/Chart.js
+++ app/scripts/charts/Chart.js
@@ -1,23 +1,105 @@
 'use strict';
 
-function Chart(data, config) {
-  this.data = data;
+function setupGraph(config) {
   this.chart = {
     height: config.chartHeight - config.margin.top - config.margin.bot,
-    width: config.chartWidth - config.margin.left - config.margin.right,
-    scale: generateScales.call(this, data)
+    width: config.chartWidth - config.margin.left - config.margin.right
   };
+  this.scale = generateScales.call(this);
+  this.axis = generateAxis.call(this);
 }
 
-function generateScales(data){
+function generateSVG() {
+  var self = this;
+  var chart = self.chart;
+  var margin = self.config.margin;
+
+  var svg = d3.select(self.config.elementID)
+      .append('svg')
+      .attr({
+        'width': chart.width + margin.left + margin.right,
+        'height': chart.height + margin.top + margin.bot,
+      })
+      .append('g')
+      .attr('transform', 'translate('+margin.left+','+margin.top+')');
+
+  svg.append('g')
+    .attr('class', 'grid axis')
+    .call(self.axis.yAxis);
+
+  return svg;
+}
+
+function generateAxisLines(svg, chart) {
+  // X & Y AXIS
+  var axis = svg.append('g').attr('class', 'axis');
+  axis.append('line')
+    .attr({
+      'y1': chart.height,
+      'x2': chart.width,
+      'y2': chart.height,
+      'stroke-width': 1,
+      'class': 'x'
+    });
+  axis.append('line')
+    .attr({
+      'y2': chart.height,
+      'stroke-width': 1,
+      'class': 'y'
+    });
+
+  return axis;
+}
+
+function generateScales() {
   return {
     'xScale': d3.scale.ordinal()
-      .domain(data.map(function(d) { return d.label; }))
+      .domain(this.data.map(function(d) { return d.label; }))
       .rangeRoundBands([0, this.chart.width], 0.5),
     'yScale': d3.scale.linear()
-      .domain([0, d3.max(data, function(d) { return d.data; })])
+      .domain([0, d3.max(this.data, function(d) { return d.data; })])
       .range([this.chart.height, 0])
   };
 }
+
+function generateTicks() {
+  return d3.svg.axis()
+    .scale(this.scale.yScale)
+    .orient('left')
+    .ticks(this.config.axis.y.ticks)
+    .outerTickSize(0)
+    .tickSize(-this.chart.width);
+}
+
+function generateAxis() {
+  return {
+    'yAxis': generateTicks.call(this),
+    'xAxis': null
+  };
+}
+
+function endAll(transition, callback) {
+  var n;
+  if (transition.empty()) {
+    callback();
+  } else {
+    n = transition.size();
+    transition.each('end', function () {
+      n--;
+      if (n === 0) {
+        callback();
+      }
+    });
+  }
+}
+
+var Chart = {
+  setupGraph: setupGraph,
+  generateSVG: generateSVG,
+  generateAxisLines: generateAxisLines,
+  generateScales: generateScales,
+  generateAxis: generateAxis,
+  endAll: endAll
+};
 
 module.exports = Chart;
