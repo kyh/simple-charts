4b4ddefa5cd0c881c09fa8c87a2f7133d89875d3
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-06-06 00:17:28 -0400

    Begin integrating browserify

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
-/* jshint devel:true */
 'use strict';
+
 var BarChart = require('./charts/Bar.js');
 
 var data = [{
@@ -49,66 +49,19 @@ var config = {
 };
 
 var barChart = new BarChart(data, config);
-  // var barChart = new Chart.BarChart(chartData, barChartConfig);
 
-  // (function updateChart(d){
-  //   var updatedData = d || chartData.concat([
-  //     { label: 'Aug', data: 70 },
-  //     { label: 'Sep', data: 14 }
-  //   ]);
-  //   setTimeout(function(){
-  //     barChart.updateData(updatedData);
-  //     if (updatedData.length < 4) {
-  //       updateChart(chartData);
-  //     } else {
-  //       updateChart(updatedData.slice(~~(Math.random() * updatedData.length - 2)));
-  //     }
-  //   }, ~~(Math.random() * 5000) + 5000);
-  // })();
-
-  // // Line Chart
-  // var lineChartConfig = {
-  //   elementID: '#line-chart',
-  //   chartWidth: $('.line-chart-container')[0].offsetWidth,
-  //   chartHeight: 400,
-  //   axis: {
-  //     y: {
-  //       ticks: 8
-  //     }
-  //   },
-  //   classes: {
-  //     group: 'group',
-  //     point: 'point',
-  //     line: 'line',
-  //     label: 'labels'
-  //   },
-  //   margin: {
-  //       top: 20,
-  //       bot: 30,
-  //       left: 30,
-  //       right: 0
-  //   }
-  // };
-
-  // var lineChart = new Chart.LineChart(chartData, lineChartConfig);
-
-  // // Pie Chart
-  // var pieChartData = [{
-  //   label: 'Complete',
-  //   data: 77
-  // }];
-
-  // var pieChartConfig = {
-  //   elementID: '#pie-chart',
-  //   chartWidth: $('.pie-chart-container')[0].offsetWidth,
-  //   chartHeight: 400,
-  //   margin: {
-  //       top: 0,
-  //       bot: 0,
-  //       left: 0,
-  //       right: 0
-  //   }
-  // };
-
-  // var pieChart = new Chart.PieChart(pieChartData, pieChartConfig);
+(function updateChart(d){
+  var updatedData = d || data.concat([
+    { label: 'Aug', data: 70 },
+    { label: 'Sep', data: 14 }
+  ]);
+  setTimeout(function(){
+    barChart.updateData(updatedData);
+    if (updatedData.length < 4) {
+      updateChart(data);
+    } else {
+      updateChart(updatedData.slice(~~(Math.random() * updatedData.length - 2)));
+    }
+  }, ~~(Math.random() * 5000) + 5000);
+})();
 
