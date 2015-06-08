e1ceec3481f3abe921b8cd8326bacd38841a5445
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-06-07 22:42:49 -0400

    Added copy function to gulp build

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -5,5 +5,6 @@ var rev = require('gulp-rev');
 
 // Build
 gulp.task('build', ['html', 'styles', 'vendor', 'browserify', 'images'], function() {
-
+  return gulp.src(['.tmp/*.html', '.tmp/styles/*.css', '.tmp/scripts/*.js'], {base: '.tmp'})
+    .pipe(gulp.dest('dist'));
 });
